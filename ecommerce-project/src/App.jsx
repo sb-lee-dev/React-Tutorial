5837f0cc4f3bbbc943be51f081b55bbec808570a
Seongbae Lee <slee617seneca@gmail.com>
2026-04-22 13:19:09 +0900

    Refactor structure of the project
    put checkout components into one folder and make naming convention consistent.

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router";
 import { HomePage } from "./pages/HomePage";
 import "./App.css";
-import { CheckoutPage } from "./pages/CheckoutPage";
+import { CheckoutPage } from "./pages/checkout/CheckoutPage";
 import { OrdersPage } from "./pages/OrdersPage";
 import { TrackingPage } from "./pages/TrackingPage";
 
