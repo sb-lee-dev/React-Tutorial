b6e0b93fdb2d73735035a1c7449e987bdca96e3f
Seongbae Lee <slee617seneca@gmail.com>
2026-04-23 09:44:33 +0900

    Use axios to fetch the data

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,15 +1,12 @@
+import axios from "axios";
 import { Header } from "../components/Header";
 import { products } from "../../staring-code/data/products";
 import "./HomePage.css";
 
 export function HomePage() {
-  fetch("http://localhost:3000/api/products")
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      console.log(data);
-    });
+  axios.get("http://localhost:3000/api/products").then((response) => {
+    console.log(response.data);
+  });
 
   return (
     <>
