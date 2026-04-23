afce8965d024fdbf348bce915046c405093fa278
Seongbae Lee <slee617seneca@gmail.com>
2026-04-23 09:50:07 +0900

    Use data from the backend to generate the html
    Use data from the backend to generate the html

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,12 +1,16 @@
 import axios from "axios";
+import { useState, useEffect } from "react";
 import { Header } from "../components/Header";
-import { products } from "../../staring-code/data/products";
 import "./HomePage.css";
 
 export function HomePage() {
-  axios.get("http://localhost:3000/api/products").then((response) => {
-    console.log(response.data);
-  });
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/api/products").then((response) => {
+      setProducts(response.data);
+    });
+  }, []);
 
   return (
     <>
